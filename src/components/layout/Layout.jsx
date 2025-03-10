f1074e9d242fffca9c22f6e4348ef2b3980e55de
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2025-03-10 00:32:52 -0500

    individual pages with their own color figured out

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Header from "../header/Header.jsx";
 import Footer from "../footer/Footer.jsx";
 
-export default function Layout({ title, navbarPosition, children }) {
+export default function Layout({ title, navbarPosition, children, theme}) {   console.log("Theme applied:", theme);  // Checks if theme is being applied for each page 
+
   return (
-    <div className="layout">
+    <div className= {`layout ${theme}`}> {/*Apply theme as a class*/}
       <Header title={title} navbarPosition={navbarPosition} />
       <main>
         {children}
