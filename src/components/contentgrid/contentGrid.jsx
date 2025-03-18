a455352b6b28a5cb93fa6df03b0d15ace6b378df
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2025-03-18 15:21:24 -0500

    flip image

--- src/components/contentgrid/contentGrid.jsx
+++ src/components/contentgrid/contentGrid.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "./contentGrid.css"
 
-export default function contentGrid ({items}) {
+export default function ContentGrid ({items}) {
     return (
         <div className ="grid-container">
             {items.map((item, index)=>(
+
                 <div className="grid-item" key={index}>
+                        <div className="flip-card-inner">
+                            
+                            {/*  front side image and name */}
+                           <div className="flip-card-front">   
                     <img src={item.image} alt={item.title} className="grid-image"/> 
-                    <h3>
+                    <h3 className="grid-title">
                         {item.title}
                     </h3>
-                    <p>{item.description}</p>
                     </div>
-                ))}
-        </div>
-    )
+
+                    {/* back side */}
+                    <div className="flip-card-back">
+                        <p>{item.description}</p>
+                    </div>
+
+                </div>
+            </div>
+        ))}
+    </div>
+);
 }
